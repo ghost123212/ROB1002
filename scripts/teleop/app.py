75269ae8aac13bba215f3ecf46e18fc062507bde
Josh Cooper <joshcooper@lincoln.ac.uk>
2023-12-13 12:48:05 +0000

    Added hostname functionality

--- scripts/teleop/app.py
+++ scripts/teleop/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, send_from_directory
 from flask_sock import Sock
+import socket 
 
 app = Flask(__name__)
 sock = Sock(app)
 
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    hostname = (socket.gethostname().split(".")[0]).upper()
+    return render_template('index.html', hostname=hostname)
 
 @app.route("/manifest.json")
 def manifest():
