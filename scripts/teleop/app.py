5a43550ab0e1d69d6d69769a44ff3fa22094e48d
Josh Cooper <joshcooper@lincoln.ac.uk>
2023-12-13 17:14:56 +0000

    Added Trilobot Support

--- scripts/teleop/app.py
+++ scripts/teleop/app.py
@@ -2,11 +2,20 @@ from flask import Flask, render_template, send_from_directory, Response
 from flask_sock import Sock
 import socket 
 import cv2
+# from picamera2 import Picamera2
 
 app = Flask(__name__)
 sock = Sock(app)
 
+import time
+from trilobot import Trilobot
 
+speed = 0.5
+tbot = Trilobot()
+
+# picam2 = Picamera2()
+# picam2.configure(picam2.create_preview_configuration(main={"format": 'BGR888', "size": (640, 480)})) # opencv works in BGR not RGB
+# picam2.start()
 
 @app.route('/')
 def index():
@@ -17,16 +26,35 @@ def index():
 def manifest():
     return send_from_directory('./static', 'manifest.json')
 
-@sock.route('/echo')
-def echo(sock):
+@app.route("/app.js")
+def script():
+    return send_from_directory('./static', 'app.js')
+
+@sock.route('/command')
+def command(sock):
     while True:
-        data = sock.receive()
-        sock.send(data)
+        cmd = sock.receive()
+
+        if cmd == "left":
+            tbot.curve_forward_left(speed)
+
+        elif cmd == "right":
+            tbot.curve_forward_right(speed)
+
+        elif cmd == "up":
+            tbot.forward(speed)
+
+        elif cmd == "down":
+            tbot.forward(speed)
+
+        else: 
+            print("send either `up` `down` `left` or `right` to move your robot!")
 
 # From https://www.aranacorp.com/en/stream-video-from-a-raspberry-pi-to-a-web-browser/
 def video_gen():
     """Video streaming generator function."""
     vs = cv2.VideoCapture(0)
+    # vs = picam2.capture_array()
     while True:
         ret,frame=vs.read()
         ret, jpeg = cv2.imencode('.jpg', frame)
@@ -36,7 +64,7 @@ def video_gen():
         
     vs.release()
     cv2.destroyAllWindows() 
-    
+
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
