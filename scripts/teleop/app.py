5a7f4cdcb83a80ab943fe51fb16d71fc658818f4
Jonathan Cox <jcox289@gmail.com>
2023-12-14 15:39:24 +0000

    Added web teleop.

--- scripts/teleop/app.py
+++ scripts/teleop/app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, send_from_directory, Response
 from flask_sock import Sock
 import socket 
 import cv2
-# from picamera2 import Picamera2
+from picamera2 import Picamera2
+import numpy as np
 
 app = Flask(__name__)
 sock = Sock(app)
@@ -13,9 +14,11 @@ from trilobot import Trilobot
 speed = 0.5
 tbot = Trilobot()
 
-# picam2 = Picamera2()
-# picam2.configure(picam2.create_preview_configuration(main={"format": 'BGR888', "size": (640, 480)})) # opencv works in BGR not RGB
-# picam2.start()
+enable_colour_detect = False
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'BGR888', "size": (640, 480)})) # opencv works in BGR not RGB
+picam2.start()
 
 @app.route('/')
 def index():
@@ -32,44 +35,76 @@ def script():
 
 @sock.route('/command')
 def command(sock):
+    
     while True:
-        cmd = sock.receive()
+        cmd = sock.receive().split(':')
 
-        if cmd == "left":
+        if cmd[0] == "left":
             tbot.curve_forward_left(speed)
 
-        elif cmd == "right":
+        elif cmd[0] == "right":
             tbot.curve_forward_right(speed)
 
-        elif cmd == "up":
+        elif cmd[0] == "up":
             tbot.forward(speed)
 
-        elif cmd == "down":
-            tbot.forward(speed)
+        elif cmd[0] == "down":
+            tbot.backward(speed)
+
+        elif cmd[0] == "stop":
+            tbot.stop()
+
+        elif cmd[0] == "speed":
+            speed = float(cmd[1])
 
         else: 
-            print("send either `up` `down` `left` or `right` to move your robot!")
+            print("send either `up` `down` `left` `right` or `stop` to move your robot!")
+
+def colour_detect(_img):
+    hsv_img = cv2.cvtColor(_img, cv2.COLOR_BGR2HSV) # convert to hsv image
+
+    # Create a binary (mask) image, HSV = hue (colour) (0-180), saturation  (0-255), value (brightness) (0-255)
+    hsv_thresh = cv2.inRange(hsv_img,
+                                np.array((50, 0, 0)), # lower range
+                                np.array((80, 255, 255))) # upper range
+
+    # Find the contours in the mask generated from the HSV image
+    hsv_contours, hierachy = cv2.findContours(
+        hsv_thresh.copy(),
+        cv2.RETR_TREE,
+        cv2.CHAIN_APPROX_SIMPLE)
+    
+        
+    # In hsv_contours we now have an array of individual closed contours (basically a polgon around the blobs in the mask). Let's iterate over all those found contours.
+    for c in hsv_contours:
+        # This allows to compute the area (in pixels) of a contour
+        a = cv2.contourArea(c)
+        # and if the area is big enough, we draw the outline
+        # of the contour (in blue)
+        if a > 100.0:
+            cv2.drawContours(_img, c, -1, (255, 0, 0), 10)
+
+    return _img
 
 # From https://www.aranacorp.com/en/stream-video-from-a-raspberry-pi-to-a-web-browser/
 def video_gen():
     """Video streaming generator function."""
-    vs = cv2.VideoCapture(0)
-    # vs = picam2.capture_array()
     while True:
-        ret,frame=vs.read()
-        ret, jpeg = cv2.imencode('.jpg', frame)
+        img = picam2.capture_array()
+        if enable_colour_detect:
+            img = colour_detect(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # convert back to rgb image
+        ret, jpeg = cv2.imencode('.jpg', img)
         frame=jpeg.tobytes()
         yield (b'--frame\r\n'
         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        
-    vs.release()
-    cv2.destroyAllWindows() 
 
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(video_gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port =5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port =5000, debug=False, threaded=True)
+    tbot.stop()
+    print("Trilobot stopped.")
--- scripts/teleop_opencv/static/app.js
+++ scripts/teleop_opencv/static/app.js
@@ -41,6 +41,11 @@ document.getElementById('stop-mobile').onclick = ev => {
     sendCmd("stop");
 };
 
+document.getElementById('opencv').onclick = ev => {
+    ev.preventDefault();
+    sendCmd("opencv");
+};
+
 // Slider Controls
 const speedSlider = document.getElementById('speedSlider');
 
@@ -49,6 +54,59 @@ speedSlider.addEventListener('input', () => {
     speedLabel.textContent = 'Speed (0-1): ' + speedSlider.value;
     // Send the speed value to the server
     sendCmd(`speed:${speedSlider.value}`);
+});
+
+// Box inputs
+hueMinInput.addEventListener('input', () => {
+    const newHueMin = parseFloat(hueMinInput.value);
+    
+    if (!isNaN(newHueMin)) {
+        // Send the value to the server
+        sendCmd(`hue_min:${newHueMin}`);
+    }
+});
+
+hueMaxInput.addEventListener('input', () => {
+    const newHueMax = parseFloat(hueMaxInput.value);
+    
+    if (!isNaN(newHueMax)) {
+        // Send the value to the server
+        sendCmd(`hue_max:${newHueMax}`);
+    }
+});
+saturationMinInput.addEventListener('input', () => {
+    const newsaturationMin = parseFloat(saturationMinInput.value);
+    
+    if (!isNaN(newsaturationMin)) {
+        // Send the value to the server
+        sendCmd(`saturation_min:${newsaturationMin}`);
+    }
+});
+
+saturationMaxInput.addEventListener('input', () => {
+    const newsaturationMax = parseFloat(saturationMaxInput.value);
+    
+    if (!isNaN(newsaturationMax)) {
+        // Send the value to the server
+        sendCmd(`saturation_max:${newsaturationMax}`);
+    }
+});
+intensityMinInput.addEventListener('input', () => {
+    const newintensityMin = parseFloat(intensityMinInput.value);
+    
+    if (!isNaN(newintensityMin)) {
+        // Send the value to the server
+        sendCmd(`intensity_min:${newintensityMin}`);
+    }
+});
+
+intensityMaxInput.addEventListener('input', () => {
+    const newintensityMax = parseFloat(intensityMaxInput.value);
+    
+    if (!isNaN(newintensityMax)) {
+        // Send the value to the server
+        sendCmd(`intensity_max:${newintensityMax}`);
+    }
 });
 
 // WASD
